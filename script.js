26e0ec53f116bfdf573a71f5b4127fb8f18b01e0
ziyuezhu <zhuziyue94@gamil.com>
2026-01-27 20:15:06 -0500

    skeleton

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title,author,pages,read){
+    if (!new.target){
+        throw Error ("You must use the 'new' operator to call the constructor")
+    }
+
+    this.title = title;
+    this.author = author
+    this.pages = pages
+    this.read= read
+    this.info = function(){
+        console.log(`The ${this.title} by ${author},${pages},${this.read}`)
+    }
+    return this.info
+}
